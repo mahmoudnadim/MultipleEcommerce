aba6efa30b6c05d717e94d908061fb01bf3b0c73
mahmoudnadim <mahmoudnadim51@gmail.com>
2025-09-18 00:02:39 +0300

    Update

--- src/Sections/AboutProject/AboutProject.jsx
+++ src/Sections/AboutProject/AboutProject.jsx
@@ -1,5 +1,5 @@
 import "./AboutProject.css"
-import { MdOutlineDesignServices,MdOutlineSecurity } from "react-icons/md";
+import { MdOutlineDesignServices,MdOutlineSecurity,MdOutlineTranslate  } from "react-icons/md";
 import DCard from "../../components/DCard/DCard"
 import { HiOutlineRocketLaunch } from "react-icons/hi2"
 
@@ -11,6 +11,7 @@ export default function AboutProject() {
           <DCard icon={<MdOutlineDesignServices />} title="Simple and Flexible:" desc="Designed for ease of use with a clean, intuitive interface."/>
           <DCard icon={<MdOutlineSecurity />} title="Security & Oversight:" desc="Strong protection and monitoring for your financial system."/>
           <DCard icon={<HiOutlineRocketLaunch />} title="Fast & Accurate:" desc="Delivers high speed and precise performance."/>
+          <DCard icon={<MdOutlineTranslate  />} title="Website Translator" desc="Break language barriers with our powerful Website Translator."/>
       </div>
     </div>
   )
--- src/Sections/Hero/Hero.jsx
+++ src/Sections/Hero/Hero.jsx
@@ -7,6 +7,7 @@ export default function Hero() {
         <div className="heroLeft">
             <p className="heroTitle">Your Marketplace, Your Rules</p>
             <p className="heroDesc">Open a store, sell products, and get paid online—fast and easy.</p>
+             <a href="https://fashionecommerce1.vercel.app" className="g-btn heroBtn" target="_blank">See Template</a>
         </div>
         <div className="heroRight rounded-xl overflow-hidden">
             <img src={heroImg} alt="" />
